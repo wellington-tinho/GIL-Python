29a6b30f02aa377cb744c50f9dca038bdfef174f
wrtinho <tinho1068@hotmail.com>
2020-08-01 17:30:56 -0300

    feat: includes py cods

---
+++ single_thread.py
@@ -0,0 +1,18 @@
+# single_threaded.py
+import time
+from threading import Thread
+
+COUNT = 50000000
+
+def countdown(n):
+    while n>0:
+        n -= 1
+
+start = time.time()
+countdown(COUNT)
+end = time.time()
+
+print('Time taken in seconds -', end - start)  
+
+
+# output = 'Time taken in seconds - ' 3.3819386959075928
